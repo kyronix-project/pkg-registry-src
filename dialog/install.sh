cb6d92694698b534c9efa5cf729b33c3f13b1a38
Zennix <101998415+z3nnix@users.noreply.github.com>
2026-07-23 19:44:30 +0300

    fix: add execute permissions to dialog installation script

--- dialog/install.sh
+++ dialog/install.sh
@@ -5,3 +5,5 @@ install_dir="$4"
 mkdir -p "$install_dir"
 cp "$1/payload/bin/dialog" "$install_dir/dialog"
 cp "$1/payload/lib/libdialog.a" "$install_dir/libdialog.a"
+
+chmod 775 "$install_dir/dialog"
